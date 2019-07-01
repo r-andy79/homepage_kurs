0ad17adf65d8eab48e1082a7686ab10fa1abd550
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2019-07-01 13:28:59 +0100

    class naming convention changed to BEM

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const name = "Andrzej";
+let age = 40;
+
+
 console.log('Siema! Witaj na mojej stronie :). Umiemy korzystać z konsoli, co? ;)')
+
+const about = document.querySelector('.about__paragraph--js')
+console.log(about.innerHTML);
+about.innerHTML = `<strong>Nazywam się</strong> ${name} i mam ${age} lat`
+
+const paragraphs = document.querySelectorAll('p')
+console.log(paragraphs)
+console.log(paragraphs[0])
